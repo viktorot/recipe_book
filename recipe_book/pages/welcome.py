eda9caba5c93d9ce082550dc7865f56958fd57cf
Viktor Chuchurski <viktor@ssrd.io>
2020-12-29 09:59:21 -0800

    - session required for welcome page

--- recipe_book/pages/welcome.py
+++ recipe_book/pages/welcome.py
@@ -1,4 +1,21 @@
 from django.views.generic import TemplateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from ..views import initialize_context
 
-class WelcomePage(TemplateView):
-    template_name = 'welcome.html'
+class OAuthLoginRequiredMixin(LoginRequiredMixin):
+
+  def dispatch(self, request, *args, **kwargs):
+    user = request.session.get('user', {'is_authenticated': False})
+
+    if not user['is_authenticated']:
+      return self.handle_no_permission()
+
+    return super(LoginRequiredMixin, self).dispatch(
+        request, *args, **kwargs)
+
+class WelcomePage(OAuthLoginRequiredMixin, TemplateView):
+  template_name = 'welcome.html'
+  login_url = '/signin'
+
+  def get_context_data(self, **kwargs):
+    return initialize_context(self.request)
